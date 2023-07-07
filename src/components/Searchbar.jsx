ad33fd339491a041df23c8a78239dc22597ac6b1
Maaya <yamauchimaaya@gmail.com>
2023-07-06 21:52:25 -0500

    started working on searchbar and added magnifying glass

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,28 @@
+import {useState} from "react";
+import '../App.css'
+
+
 function Searchbar() {
 
+    const [input, setInput] = useState("")
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+      };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-1/3">
+        <input
+          className="px-2 py-3 w-full rounded-full bg-white border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 search-input"
+          type="text"
+          value={input}
+          placeholder="Search for your market here"
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+    )
 }
 
 export default Searchbar
